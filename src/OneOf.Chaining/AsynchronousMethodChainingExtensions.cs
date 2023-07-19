1a7c3eb78b9e59597fd6a54e4a4c0e5e99837a76
Andrew Poole <andrew.poole@clear.bank>
2023-07-19 13:17:20 +0100

    added overload and additional tests

--- src/OneOf.Chaining/AsynchronousMethodChainingExtensions.cs
+++ src/OneOf.Chaining/AsynchronousMethodChainingExtensions.cs
@@ -24,7 +24,37 @@ public static class AsynchronousMethodChainingExtensions
             return TOrFailure;
 
         // ...Otherwise do your next job
-        return await nextJob(TOrFailure.AsT0);
+        var currentT = TOrFailure.AsT0;
+        return await nextJob(currentT);
+    }
+
+    /// <summary>
+    /// Extension method which enables method chaining. Each method in the chain must accept a T and return a Task of OneOf of T or TFailure.<br/>
+    /// The result of the currentJob Task is evaluated and if it contains a T (signifying success), the result is passed to the nextJob Func to be executed.<br/>
+    /// If at any point in the chain, the result of the currentJob Task contains a TFailure, then that TFailure is immediately passed through the chain, i.e. no more nextJob Func's are executed.
+    /// </summary>
+    /// <typeparam name="T">Represents success, also likely contains any state needed to perform any operations and possibly store any results from processing in the chain.</typeparam>
+    /// <typeparam name="TFailure">Represents a failure at some point in the chain.</typeparam>
+    /// <param name="currentJobResult">The resulting Task of the previous link in the chain.</param>
+    /// <param name="nextJob">A Func containing the next piece of work in the chain.</param>
+    /// <param name="onFailure">An Action which will be invoked if nextJob fails, before TFailure is returned.</param>
+    /// <returns></returns>
+    public static async Task<OneOf<T, TFailure>> Then<T, TFailure>(this Task<OneOf<T, TFailure>> currentJobResult, Func<T, Task<OneOf<T, TFailure>>> nextJob, Func<T, Task> onFailure)
+    {
+        // Inspect result of (probably already awaited) currentJobResult, if its a TFailure return it...
+        var TOrFailure = await currentJobResult;
+        if (TOrFailure.IsT1)
+            return TOrFailure;
+
+        // ...Otherwise do your next job ... 
+        var currentT = TOrFailure.AsT0;
+        var result = await nextJob(currentT);
+
+        if (result.IsT0)
+            return result;
+
+        await onFailure(currentT);
+        return result;
     }
 
     /// <summary>
--- tests/OneOf.Chaining.Tests/AsynchronousMethodChainingTests.cs
+++ tests/OneOf.Chaining.Tests/AsynchronousMethodChainingTests.cs
@@ -41,7 +41,7 @@ public class AsynchronousMethodChainingTests
     }
 
     [Test]
-    public async Task A_chain_of_thens_TFailure_as_soon_as_a_job_fails()
+    public async Task A_chain_of_thens_returns_TFailure_as_soon_as_a_job_fails()
     {
         async Task<OneOf<StateStore, Error>> Job1(StateStore s)
         {
@@ -71,6 +71,64 @@ public class AsynchronousMethodChainingTests
             .Then(s => Job3(s));
 
         Assert.That(result.IsT1, Is.True);
+    }
+
+    [Test]
+    public async Task In_a_chain_of_thens_onFailure_is_called_if_a_job_fails_and_then_the_Failure_is_returned()
+    {
+        var job1OnFailureCalled = false;
+        var job2OnFailureCalled = false;
+        var job3OnFailureCalled = false;
+
+        async Task<OneOf<StateStore, Error>> Job1(StateStore s)
+        {
+            await Task.Delay(100);
+            s.Flag1 = true;
+            return s;
+        }
+
+        async Task Job1OnFailure(StateStore s)
+        {
+            await Task.Delay(100);
+            job1OnFailureCalled = true;
+        }
+
+        async Task<OneOf<StateStore, Error>> Job2(StateStore s)
+        {
+            await Task.Delay(100);
+            return new Error();
+        }
+
+        async Task Job2OnFailure(StateStore s)
+        {
+            await Task.Delay(100);
+            job2OnFailureCalled = true;
+        }
+
+        async Task<OneOf<StateStore, Error>> Job3(StateStore s)
+        {
+            await Task.Delay(100);
+            s.Flag3 = true;
+            throw new Exception("Job3 should not be run");
+        }
+
+        async Task Job3OnFailure(StateStore s)
+        {
+            await Task.Delay(100);
+            job3OnFailureCalled = true;
+        }
+
+        Task<OneOf<StateStore, Error>> Create() => Task.FromResult((OneOf<StateStore, Error>)new StateStore());
+
+        var result = await Create()
+            .Then(Job1, Job1OnFailure)
+            .Then(Job2, Job2OnFailure)
+            .Then(Job3, Job3OnFailure);
+
+        Assert.That(result.IsT1, Is.True);
+        Assert.That(job1OnFailureCalled, Is.False);
+        Assert.That(job2OnFailureCalled, Is.True);
+        Assert.That(job3OnFailureCalled, Is.False);
     }
 
     [Test]
